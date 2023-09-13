e2265c4336a574a08e539047fc79aa338bf2f70c
Rente6 <84778080+Rente6@users.noreply.github.com>
2023-09-13 16:14:43 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Img from "./assets/🦆 icon _User Circle icon_.png";
+
+function App() {
+  return (
+    <body className="CentroXY">
+      <div className="esperandomenudorafael"> .</div>
+      <div className="esperandomenudorafael2"> .</div>
+      <h1>Centro Profissional</h1>
+
+      <img src={Img}/>
+
+      <h2>Doutor/a</h2>
+      <h3>Biografia:</h3>
+      <p>
+        Aqui o profissional irá colocar uma descrição sobre seus estudos,
+        carreira e experiencia.
+      </p>
+    </body>
+  );
+}
+
+export default App;
